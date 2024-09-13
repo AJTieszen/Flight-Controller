a85710d14e61ed5f7e72f2dd06f3d40a12abd7fb
Andrew Tieszen <pilottzn@gmail.com>
2024-09-13 10:52:17 -0600

    Update Flight_Controller.ino.ino

--- Flight_Controller.ino/Flight_Controller.ino.ino
+++ Flight_Controller.ino/Flight_Controller.ino.ino
@@ -4,6 +4,7 @@ Joystick_ joystick;
 // Analog Pin Definitions
 #define THROTTLE_L 0
 #define THROTTLE_R 1
+#define THROTTLE_MAX 768
 
 // Digital Pin Definitions
 
@@ -14,9 +15,20 @@ int throttle_r = 0;
 
 void setup() {
   // Start Gamepad Emulation
-  joystick.begin();
+  //joystick.begin();
+  Serial.begin(9600);
+
+  // Configure gamepad mode and settings
+  //joystick.setThrottleRange(0, 768);
 }
 
 void loop() {
-  // Read In
+  // Read Inputs
+  throttle_l = analogRead(THROTTLE_L);
+  throttle_r = analogRead(THROTTLE_R);
+
+  // Send Controller State
+  //joystick.setThrottle(throttle_l);
+  Serial.println(throttle_l);
+  delay(10);
 }
