cffae1fa117a7355039337752787e6b335460eb6
Andrew Tieszen <pilottzn@gmail.com>
2024-09-13 10:04:10 -0600

    Added joystick library

--- Flight_Controller.ino/Flight_Controller.ino.ino
+++ Flight_Controller.ino/Flight_Controller.ino.ino
@@ -1,9 +1,18 @@
+
+
+int led_delta = 1;
+int led_intensity = 0;
+
 void setup() {
-  pinMode(LEDBUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LEDBUILTIN, HIGH);
-  delay(250);
-  digitalWrite
+  analogWrite(LED_BUILTIN, led_intensity);
+  led_intensity += led_delta;
+
+  if(led_intensity >= 255) led_delta = -1;
+  if(led_intensity <= 0) led_delta = 1;
+
+  delay(5);
 }
